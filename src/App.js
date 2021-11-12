05f269cd701f8a16974189a8ffa3f076a33a0f82
Jimmy Chuk <jimmychuk@outlook.com>
2021-11-11 20:16:26 -0500

    portal modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
@@ -16,10 +16,11 @@ function App() {
   };
 
   return (
-    <div>
+    //using fragments <React.Fragment> or <Fragment>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
